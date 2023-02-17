88b087403bdbef43d2eef5174e443ac049a86b76
Simmo Saan <simmo.saan@gmail.com>
2023-02-17 16:13:54 +0200

    Lahenda crashcourse praktikumis

--- src/crashcourse/basics.ml
+++ src/crashcourse/basics.ml
@@ -4,12 +4,12 @@
 
 (** Suurendab täisarvu ühe võrra. *)
 let inc (x: int): int =
-  failwith "TODO"
+  x + 1
 
 (** Suurendab täisarvu ühe võrra.
     Kasutada "lambdafunktsiooni". *)
-let inc': int -> int = fun x ->
-  failwith "TODO"
+let inc': int -> int =
+  fun x -> x + 1
 
 
 (** Rekursiivsed funktsioonid. *)
@@ -18,12 +18,20 @@ let inc': int -> int = fun x ->
     Kasutada if-i.
     Vihje: võrdusoperaator on = (mitte ==) ja selle vastand on <> (mitte !=). *)
 let rec fact (n: int): int =
-  failwith "TODO"
+  if n = 0 then
+    1
+  else
+    n * fact (n - 1)
+
+(* a ? b : c *)
+(* if a then b else c *)
 
 (** Arvutab faktoriaali.
     Kasutada match-i. *)
 let rec fact' (n: int): int =
-  failwith "TODO"
+  match n with
+  | 0 -> 1
+  | n -> n * fact' (n - 1)
 
 
 (** Mitme argumendiga funktsioonid. *)
@@ -32,12 +40,19 @@ let rec fact' (n: int): int =
     Vt. https://en.wikipedia.org/wiki/Euclidean_algorithm#Implementations.
     Vihje: Jäägi operaator (mitte funktsioon) on mod. *)
 let rec gcd (x: int) (y: int): int =
-  failwith "TODO"
+  if y = 0 then
+    x
+  else
+    gcd y (x mod y)
 
 (** Arvutab implikatsiooni.
     Kasutada mitme argumendiga match-i. *)
 let implies (x: bool) (y: bool): bool =
-  failwith "TODO"
+  match x, y with
+  | true, false -> false
+  | false, false -> true
+  | false, true -> true
+  | true, true -> true
 
 
 
